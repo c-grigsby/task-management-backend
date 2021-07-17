a105ffd3dfad4941fc545e5a6aef39fc2d511a7f
Chris Grigsby <grigsby.c@outlook.com>
2021-07-17 14:57:50 -0400

    added validation for credentials & password strength

--- src/auth/dto/auth.credentials.dto.ts
+++ src/auth/dto/auth.credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  //pass contains -> 1 upper case, 1 lower case, 1 num or special char,else error msg
+  @Matches(/((?=.*\d)|(?=.\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
